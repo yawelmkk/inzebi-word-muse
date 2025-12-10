4859266acbc9102d02b43e09d80a62a2f17bb936
yawelmkk <languenzebiofficiel@gmail.com>
2025-12-10 21:26:45 +0100

    Correction finale: Import de Sonner et routage garanti

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/toaster";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "next-themes";
@@ -7,28 +8,30 @@ import WordDetail from "./pages/WordDetail";
 import NotFound from "./pages/NotFound";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Sonner } from "@/components/ui/sonner";
+// CORRECTION ICI : Remplacer l'importation nommée par l'importation par défaut (et renommé)
+import SonnerComponent from "@/components/ui/sonner";
 
 const queryClient = new QueryClient();
 
 const App = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-        <BrowserRouter basename="/inzebi-word-muse">
-          <TooltipProvider>
-            <Toaster />
-            <Sonner />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        <BrowserRouter basename="/inzebi-word-muse">
+          <TooltipProvider>
+            <Toaster />
+            <SonnerComponent /> {/* Utiliser le nom corrigé */}
 
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/word/:id" element={<WordDetail />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </TooltipProvider>
-        </BrowserRouter>
-      </ThemeProvider>
-    </QueryClientProvider>
-  );
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/word/:id" element={<WordDetail />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </TooltipProvider>
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 };
 
+export default App;
